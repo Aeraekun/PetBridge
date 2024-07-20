208d829f8b68d692fcd3e3e2836e5123af42a6f2
김재훈 <kgarry@naver.com>
2024-07-20 16:13:59 +0900

    Chore [FE] : 임시 리액트 프로젝트 초기 설정 및 패키지 설치

---
+++ FE/pet-bridge/.eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    "jest/globals": true, // Jest 테스트 환경 설정
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    // import에 대한 규칙 설정
+    "import/no-unresolved": "error",
+    "import/named": "error",
+    "import/default": "error",
+    "import/namespace": "error",
+  },
+  settings: {
+    react: {version: "detect"},
+    "import/resolver": {
+      node: {
+        // 모듈 해석을 위한 옵션 설정
+        extensions: [".js", ".jsx", ".ts", ".tsx"], // 사용할 파일 확장자
+        paths: ["./src"], // 모듈 경로
+      },
+    },
+  },
+  plugins: ["jest"], // Jest 플러그인 추가
+}
