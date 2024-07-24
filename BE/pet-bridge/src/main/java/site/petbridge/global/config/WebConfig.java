f011aed6ddeb68e71d52f9386be312192cf6b7ef
이경곤 <leeggon1213@naver.com>
2024-07-24 13:54:03 +0900

    Fix [BE] : WebConfig 파일 수정

--- BE/pet-bridge/src/main/java/site/petbridge/global/config/WebConfig.java
+++ BE/pet-bridge/src/main/java/site/petbridge/global/config/WebConfig.java
@@ -1,16 +1,13 @@
 package site.petbridge.global.config;
 
-<<<<<<< HEAD
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-=======
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
->>>>>>> 93217ee9541921f889051e59ba5f241ea9c310fd
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
