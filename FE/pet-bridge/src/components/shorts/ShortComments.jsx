4b1a08e9b7bdabe25007a984a8eb1ac667331e7e
DayoungLee <daylee4564@naver.com>
2024-07-25 00:28:14 +0900

    Feat [FE] : 쇼츠 댓글길이에 따라 더보기 버튼 구현

--- FE/pet-bridge/src/components/shorts/ShortComments.jsx
+++ FE/pet-bridge/src/components/shorts/ShortComments.jsx
@@ -5,26 +5,59 @@ import HeartIcon from "./HeartIcon"
 import SirenIcon from "./SirenIcon"
 import CommentIcon from "./CommentIcon"
 import Follow from "../../assets/image/Follow.png"
-import React, {useState} from "react"
+import React, {useState, useRef, useEffect} from "react"
 
 const Comment = ({data}) => {
-  console.log(data)
+  const [isFixedSize, setIsFixedSize] = useState(true)
+  // console.log(isFixedSize)
+  const handleToggleSize = () => {
+    setIsFixedSize(!isFixedSize)
+  }
+
+  const [showReadMore, setShowReadMore] = useState(false)
+  const contentRef = useRef(null)
+
+  useEffect(() => {
+    console.log(contentRef.current.scrollHeight)
+    // 댓글이 지정된 높이를 초과할 때 "더보기" 버튼을 표시
+    if (contentRef.current) {
+      setShowReadMore(contentRef.current.scrollHeight > 64)
+    }
+  }, [])
+
   return (
     <>
       <div className="flex space-x-2.5 px-5 py-2.5">
-        <div className="h-fit w-12 text-xl outline">
-          <img src={`../../assets/image/Siren.png`} alt="profile" />
+        <div className="h-fit w-12 mt-3  ">
+          <img src={Follow} alt="profile" />
           {/* <Image imageName={Siren.png}></Image> */}
         </div>
-        <div className="w-full">
+        <div className="w-full ">
           <div className="flex  h-7 items-center justify-between">
             <div className="flex items-center space-x-2.5">
-              <div className="  text-sm outline">{data.nickname}</div>
-              <div className="  text-sm outline">{data.regist_time}</div>
+              <div className="  text-sm  ">{data.nickname}</div>
+              <div className="  text-xs  ">
+                {data.regist_time.split("T")[0]}
+              </div>
             </div>
             <ShareIcon></ShareIcon>
           </div>
-          <div className=" h-fit w-full text-sm outline">{data.content}</div>
+          <div className="flex flex-col ">
+            <div
+              ref={contentRef}
+              className={`transition-height duration-300 ease-in-out text-sm  w-full pr-3 ${isFixedSize ? "h-10 overflow-hidden" : "h-fit"}`}
+            >
+              {data.content}
+            </div>
+            {showReadMore && (
+              <button
+                className="text-stroke rounded hover:text-black mr-3 mt-1 text-sm flex justify-end"
+                onClick={handleToggleSize}
+              >
+                {isFixedSize ? "더보기" : "닫기"}
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </>
@@ -33,14 +66,14 @@ const Comment = ({data}) => {
 
 const Profile = (data) => {
   return (
-    <div className="mx-3 flex h-16 space-x-2.5 ">
-      <div className="flex h-fit w-full items-center justify-between text-xl outline  ">
+    <div className="mx-3  flex h-16 space-x-2.5 items-center ">
+      <div className="flex h-fit w-full items-center justify-between text-xl    ">
         <div className="flex items-center space-x-2.5">
-          <div className="h-fit w-12 text-xl outline ">
-            <img src={`../../assets/image/Siren.png`} alt="profile" />
+          <div className="h-fit w-12 text-xl   ">
+            <img src={Follow} alt="profile" />
             {/* <Image imageName={Siren.png}></Image> */}
           </div>
-          <div className="text-sm outline">{data.nickname}닉네임</div>
+          <div className="text-sm  ">{data.nickname}닉네임</div>
         </div>
         <ShareIcon></ShareIcon>
       </div>
@@ -50,7 +83,7 @@ const Profile = (data) => {
 
 const Shorts = () => {
   return (
-    <div className="flex space-y-2">
+    <div className="flex space-x-2 mx-2 mt-2">
       <HeartIcon className="w-12" />
       <SirenIcon className="w-12" />
       <CommentIcon className="w-12" />
@@ -87,12 +120,12 @@ const CommentInput = () => {
       <div className="flex items-center space-x-2.5">
         <input
           type="text"
-          className="w-full outline h-10 text-sm"
+          className="w-full outline outline-stroke outline-1 h-10  rounded-md text-sm mx-2"
           placeholder="좋아요와 댓글을 남기려면 로그인하세요"
           value={inputComment}
           onChange={(e) => setInputComment(e.target.value)}
         />
-        <button className="w-12 outline h-10" onClick={sendMsg}>
+        <button className="w-12   h-10" onClick={sendMsg}>
           댓글 쓰기
         </button>
       </div>
@@ -102,7 +135,7 @@ const CommentInput = () => {
 
 const ShortsInfo = ({data}) => {
   return (
-    <div className=" mx-2 flex flex-1 w-full flex-col justify-between space-x-2.5 p-3 text-base  outline ">
+    <div className=" flex flex-1 w-full flex-col justify-between space-y-1 p-3 text-base    ">
       <div> {data.title}</div>
       <div className="text-sm"> {data.content}</div>
     </div>
@@ -112,11 +145,13 @@ const ShortsInfo = ({data}) => {
 const ShortComments = () => {
   return (
     <>
-      <div className="flex flex-col justify-between h-full ">
+      <div className="flex flex-col justify-between h-full bg-gray-50  ">
         <div className=" flex-1">
           <Profile data={dummydata[3]} />
+          <hr className="border-gray-300 my-1" />
           <ShortsInfo data={dummydata[4]}></ShortsInfo>
         </div>
+
         <ul className="flex-auto overflow-auto ">
           {dummydata.map((data, index) => (
             <li key={index}>
