68ba905dffe5b19be6424f37e92cdaeb7d534fc9
DayoungLee <daylee4564@naver.com>
2024-07-24 22:49:58 +0900

    Feat [FE] : 쇼츠 댓글창 스크롤, 플러그인, 댓글쓰기 칸

--- FE/pet-bridge/src/components/shorts/ShortComments.jsx
+++ FE/pet-bridge/src/components/shorts/ShortComments.jsx
@@ -1,6 +1,10 @@
 import dummydata from "./dummydata"
 // import Image from "./Image"
 import ShareIcon from "./ShareIcon"
+import HeartIcon from "./HeartIcon"
+import SirenIcon from "./SirenIcon"
+import CommentIcon from "./CommentIcon"
+import Follow from "../../assets/image/Follow.png"
 
 const Comment = ({data}) => {
   console.log(data)
@@ -43,17 +47,63 @@ const Profile = (data) => {
   )
 }
 
+const Shorts = () => {
+  return (
+    <div className="flex space-y-2">
+      <HeartIcon className="w-12" />
+      <SirenIcon className="w-12" />
+      <CommentIcon className="w-12" />
+      <img src={Follow} className="w-8" alt="Follow Icon" />
+    </div>
+  )
+}
+
+const CommentInput = () => {
+  return (
+    <div className="flex flex-col justify-between h-16 space-x-2.5 ">
+      <div className="flex items-center space-x-2.5">
+        <input
+          type="text"
+          className="w-full outline h-10 text-sm"
+          placeholder="좋아요와 댓글을 남기려면 로그인하세요"
+        />
+        <button className="w-12 outline h-10" onClick={Comment}>
+          댓글 쓰기
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const ShortsInfo = ({data}) => {
+  return (
+    <div className=" mx-2 flex flex-1 w-full flex-col justify-between space-x-2.5 p-3 text-base  outline ">
+      <div> {data.title}</div>
+      <div className="text-sm"> {data.content}</div>
+    </div>
+  )
+}
+
 const ShortComments = () => {
   return (
     <>
-      <Profile data={dummydata[3]} />
-      <div className=" mx-2 flex h-fit w-full flex-col justify-between space-x-2.5 p-3 text-base  outline">
-        <div> {dummydata[4].title}</div>
-        <div className="text-sm"> {dummydata[4].content}</div>
+      <div className="flex flex-col justify-between h-full ">
+        <div className=" flex-1">
+          <Profile data={dummydata[3]} />
+          <ShortsInfo data={dummydata[4]}></ShortsInfo>
+        </div>
+        <ul className="flex-auto overflow-auto ">
+          {dummydata.map((data, index) => (
+            <li key={index}>
+              <Comment data={data} />
+            </li>
+          ))}
+        </ul>
+        <div className="flex flex-1 flex-col space-y-2.5">
+          <Shorts></Shorts>
+          <CommentInput></CommentInput>
+        </div>
       </div>
-      <Comment data={dummydata[0]} />
-      <Comment data={dummydata[1]} />
-      <Comment data={dummydata[2]} />
     </>
   )
 }
