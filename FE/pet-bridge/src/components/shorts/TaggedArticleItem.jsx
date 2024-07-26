7b34f87e516e0a3f45634d66b70fba2e57b830e6
DayoungLee <120040559+2dayoung@users.noreply.github.com>
2024-07-26 17:56:49 +0900

    Feat [FE] : 쇼츠 태그된 정보

---
+++ FE/pet-bridge/src/components/shorts/TaggedArticleItem.jsx
@@ -0,0 +1,42 @@
+import DogImage from "../../assets/image/dog.png"
+
+// import React, {useState} from "react"
+
+const ArticleInfo = ({data}) => {
+  return (
+    <div className="flex w-full flex-col ">
+      <div className="flex w-full justify-between space-x-2 p-1">
+        <ArticleState state={data.process_state}></ArticleState>
+        <div className="text-sm">{data.regist_time.split("T")[0]}</div>
+      </div>
+
+      <div className="text-base">{data.title}</div>
+    </div>
+  )
+}
+const ArticleState = ({state}) => {
+  return (
+    <div className="flex size-fit items-center justify-center rounded border border-black bg-cyan-100 px-1">
+      {state}
+    </div>
+  )
+}
+
+const TagArticle = ({data}) => {
+  return (
+    <div className="flex h-24 w-full justify-between space-x-2 p-3">
+      <div className="flex w-full space-x-3">
+        <div className="flex flex-col justify-center">
+          <img
+            src={DogImage}
+            className="w-16  object-cover"
+            alt="animalImage"
+          />
+        </div>
+        <ArticleInfo data={data} />
+      </div>
+    </div>
+  )
+}
+
+export default TagArticle
