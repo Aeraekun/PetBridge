d110da86ed0a46c1f29d92e100fc29aae653e91c
이다영 <120040559+2dayoung@users.noreply.github.com>
2024-07-31 09:29:50 +0900

    Feat [FE] : 게시글목록조회,상세,작성,수정,삭제 함수 구현

--- FE/pet-bridge/src/api/boards-api.js
+++ FE/pet-bridge/src/api/boards-api.js
@@ -1,10 +1,10 @@
 import axios from "axios"
-const BASE_API_URL = "http://localhost:8080/api"
+const BASE_API_URL = "http://localhost:8080/api/boards"
 
 //게시글 다 가져오기
 export const getArticle = async () => {
   try {
-    const res = await axios.get(`${BASE_API_URL}/boards`)
+    const res = await axios.get(`${BASE_API_URL}`)
     return res.data
   } catch (e) {
     console.error(e)
@@ -15,7 +15,7 @@ export const getArticle = async () => {
 //게시글 상세 조회 (id)
 export const getArticleDetail = async (id) => {
   try {
-    const res = await axios.get(`${BASE_API_URL}/boards/${id}`)
+    const res = await axios.get(`${BASE_API_URL}/${id}`)
     console.log("getArticleDetail" + res)
     return res.data
   } catch (e) {
@@ -24,10 +24,11 @@ export const getArticleDetail = async (id) => {
   }
 }
 
+//게시글 등록
 export const registArticle = async (formData) => {
   console.log(formData)
   try {
-    const res = await axios.post(`${BASE_API_URL}/boards`, formData, {
+    const res = await axios.post(`${BASE_API_URL}`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -40,13 +41,32 @@ export const registArticle = async (formData) => {
   }
 }
 
-export const editArticle = async (id, article) => {
+//게시글 수정
+export const editArticle = async (id,formData) => {
+  console.log(formData)
   try {
-    const res = await axios.patch(`${BASE_API_URL}/boards/${id}`, article)
+    const res = await axios.patch(`${BASE_API_URL}/${id}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
     console.log("editArticle" + res)
     return res.data
   } catch (e) {
     console.error(e)
     return []
   }
+}
+
+
+//게시글 삭제
+const removeBoard = async (id)=>{
+  try {
+      const res = await axios.patch(`${BASE_API_URL}/${id}/disable`)                          
+      console.log("removeBoard" + res)
+      return  res.data
+    } catch (e) {
+      console.error(e)
+      return []                                                               
+    }
 }
