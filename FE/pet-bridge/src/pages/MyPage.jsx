d0283e14c142c798d8c7c7d92db675cd0e3c445e
김재훈 <kgarry@naver.com>
2024-08-02 12:13:30 +0900

    Feat [FE] : 마이페이지 관리자 회원, 신고 관리 화면 추가

--- FE/pet-bridge/src/pages/MyPage.jsx
+++ FE/pet-bridge/src/pages/MyPage.jsx
@@ -5,7 +5,7 @@ const MyPage = () => {
   return (
     <section className="flex h-[600px] w-[1000px] grid-cols-10 place-content-center items-center rounded-lg border">
       <MyPageNavContainer />
-      <div className="h-full grow border-l">
+      <div className="h-full grow overflow-y-auto border-l">
         <Outlet />
       </div>
     </section>
