7d8b1a8b00f5358f9acb3490d4093198fa3e9d9a
곽예빈 <ferf99@naver.com>
2024-08-05 17:20:39 +0900

    Feat [BE] : 계약서 api 유저 검증 추가

--- BE/pet-bridge/src/main/java/site/petbridge/domain/contract/dto/request/ContractRequestDto.java
+++ BE/pet-bridge/src/main/java/site/petbridge/domain/contract/dto/request/ContractRequestDto.java
@@ -1,19 +1,15 @@
 package site.petbridge.domain.contract.dto.request;
 
-import java.time.LocalDate;
-
 import lombok.Data;
 
 @Data
 public class ContractRequestDto {
 
-    private int contractorId;
-    private int contracteeId;
-    private int animalId;
-    private int month;
-    private int payment;
-    private String content;
-    private LocalDate contractDate;
-    private LocalDate  expirationDate;
+	private int contractorId;
+	private int contracteeId;
+	private int animalId;
+	private int month;
+	private int payment;
+	private String content;
 
 }
--- BE/pet-bridge/src/main/java/site/petbridge/domain/contract/service/ContractService.java
+++ BE/pet-bridge/src/main/java/site/petbridge/domain/contract/service/ContractService.java
@@ -3,19 +3,20 @@ package site.petbridge.domain.contract.service;
 import java.util.List;
 import java.util.Optional;
 
+import site.petbridge.domain.contract.dto.request.ContractEditRequestDto;
 import site.petbridge.domain.contract.dto.request.ContractRequestDto;
 import site.petbridge.domain.contract.dto.response.ContractListResponseDto;
 import site.petbridge.domain.contract.dto.response.ContractResponseDto;
 
 public interface ContractService {
-	Optional<List<ContractListResponseDto>> getListContractByUserId(int userId);
+	Optional<List<ContractListResponseDto>> getListContractByUserId(int userId) throws Exception;
 
-	Optional<ContractResponseDto> getDetailContract(int id);
+	Optional<ContractResponseDto> getDetailContract(int id) throws Exception;
 
-	void registContract(ContractRequestDto contractRequestDto);
+	void registContract(ContractRequestDto contractRequestDto) throws Exception;
 
-	int confirmContract(int id);
+	int editContract(int id, ContractEditRequestDto contractEditRequestDto) throws Exception;
 
-	int removeContract(int id);
+	int removeContract(int id) throws Exception;
 
 }
