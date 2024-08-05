b9a5255b8b871cbc919c076cca07753632ab4bbf
이경곤 <leeggon1213@naver.com>
2024-08-05 10:45:39 +0900

    Fix [BE] : 펫픽 랜덤조회 권한 수정

--- BE/pet-bridge/src/main/java/site/petbridge/domain/board/controller/BoardController.java
+++ BE/pet-bridge/src/main/java/site/petbridge/domain/board/controller/BoardController.java
@@ -4,13 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
@@ -60,7 +54,7 @@ public class BoardController {
 		return ResponseEntity.status((HttpStatus.OK)).build();
 	}
 
-	@PatchMapping("/{id}/disable")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> removeBoard(@PathVariable("id") int boardId) {
 		if (boardService.removeBoard(boardId) == 1) {
 			return ResponseEntity.status((HttpStatus.NO_CONTENT)).build();
