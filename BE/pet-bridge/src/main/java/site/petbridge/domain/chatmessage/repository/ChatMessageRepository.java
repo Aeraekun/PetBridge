0ddfa18a69dfe51cae6713a5a0fd93f51d2f41c3
곽예빈 <ferf99@naver.com>
2024-08-05 17:52:58 +0900

    Feat [BE] : 채팅방 기본 api 추가

---
+++ BE/pet-bridge/src/main/java/site/petbridge/domain/chatmessage/repository/ChatMessageRepository.java
@@ -0,0 +1,25 @@
+package site.petbridge.domain.chatmessage.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import site.petbridge.domain.chatmessage.domain.ChatMessage;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+	@Query(value = "{ 'roomId': ?0 }", sort = "{ 'registTime': -1 }")
+	Optional<ChatMessage> findFirstByRoomIdOrderByRegistTimeDesc(int roomId);
+
+	// roomId로 필터링하고, registTime으로 정렬하여 슬라이스된 결과를 반환
+	Optional<List<ChatMessage>> findByRoomIdOrderByRegistTimeDesc(int roomId, int pageIndex, int pageSize);
+
+	Optional<List<ChatMessage>> findByRoomId(int roomId, Pageable pageable);
+	Optional<List<ChatMessage>> findByRoomId(int roomId);
+
+
+}
---
+++ BE/pet-bridge/src/main/java/site/petbridge/global/config/MongodbConfig.java
@@ -0,0 +1,19 @@
+package site.petbridge.global.config;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Configuration
+public class MongodbConfig implements InitializingBean {
+
+	@Autowired
+	private MappingMongoConverter mappingMongoConverter;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+	}
+}
