b90b0874cab1a659a74a3e06c93469e096768cb0
정규영 <w2227@naver.com>
2024-08-06 23:27:52 +0900

    Design [FE] : AnimalSearchForm, StateBadge 디자인 수정

--- FE/pet-bridge/src/components/common/Button.jsx
+++ FE/pet-bridge/src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 const Button = ({text, onClick}) => {
   return (
     <button
-      className="border-stroke bg-mild h-12 rounded-md border px-3.5 py-2.5"
+      className="h-12 rounded-md border border-stroke bg-mild px-3.5 py-2.5"
       onClick={onClick}
     >
       {text}
