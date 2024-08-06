57179aaa5a10c7f469fad1bc986b683a57b1ab68
이다영 <120040559+2dayoung@users.noreply.github.com>
2024-08-06 21:53:55 +0900

    Feat [FE] : 게시글 수정, 삭제, 등록 API연결 및 페이지네이션

--- FE/pet-bridge/src/api/boards-api.js
+++ FE/pet-bridge/src/api/boards-api.js
@@ -1,11 +1,12 @@
 import axios from "axios"
+import axiosInstance from "./axios-instance"
 const BOARD_API_URL = process.env.REACT_APP_API_URL + "/boards"
 const BOARD_COMMENTS_API_URL = process.env.REACT_APP_API_URL + "/board-comments"
 
-//게시글 다 가져오기
-export const getArticle = async () => {
+//게시글 가져오기
+export const getArticle = async (searchParams) => {
   try {
-    const res = await axios.get(`${BOARD_API_URL}`)
+    const res = await axios.get(`${BOARD_API_URL}`, {params: searchParams})
     return res.data
   } catch (e) {
     console.error(e)
@@ -30,7 +31,7 @@ export const registArticle = async (formData) => {
   console.log(formData)
   alert("등록완료")
   try {
-    const res = await axios.post(`${BOARD_API_URL}`, formData, {
+    const res = await axiosInstance.post(`${BOARD_API_URL}`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -47,7 +48,7 @@ export const registArticle = async (formData) => {
 export const editArticle = async (id, formData) => {
   console.log("editArticle", formData)
   try {
-    const res = await axios.patch(`${BOARD_API_URL}/${id}`, formData, {
+    const res = await axiosInstance.patch(`${BOARD_API_URL}/${id}`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -64,7 +65,7 @@ export const editArticle = async (id, formData) => {
 //게시글 삭제
 export const removeArticle = async (articleId) => {
   try {
-    const res = await axios.delete(`${BOARD_API_URL}/${articleId}`)
+    const res = await axiosInstance.delete(`${BOARD_API_URL}/${articleId}`)
     console.log("removeBoard" + res)
     alert("삭제 되었습니다.")
     return res.data
@@ -88,8 +89,10 @@ export const getListBoardComment = async (boardId) => {
 //댓글 작성
 export const registBoardComment = async (boardComment) => {
   try {
-    alert("댓글작성")
-    const res = await axios.post(`${BOARD_COMMENTS_API_URL}`, boardComment)
+    const res = await axiosInstance.post(
+      `${BOARD_COMMENTS_API_URL}`,
+      boardComment
+    )
     return res.data
   } catch (e) {
     console.error(e)
@@ -100,7 +103,7 @@ export const registBoardComment = async (boardComment) => {
 //댓글 수정
 export const editBoardComment = async (boardId, boardComment) => {
   try {
-    const res = await axios.patch(
+    const res = await axiosInstance.patch(
       `${BOARD_COMMENTS_API_URL}/${boardId}`,
       boardComment
     )
@@ -111,10 +114,12 @@ export const editBoardComment = async (boardId, boardComment) => {
   }
 }
 
+//댓글 삭제
+
 export const removeBoardComment = async (id) => {
   try {
     alert("댓글 삭제")
-    const res = await axios.patch(`${BOARD_COMMENTS_API_URL}/${id}/disable`)
+    const res = await axiosInstance.delete(`${BOARD_COMMENTS_API_URL}/${id}`)
     console.log("removeBoardComment" + res)
     return res.data
   } catch (e) {
