4cdc391d2018f1ad221d5badcec91a93a130e19c
이다영 <120040559+2dayoung@users.noreply.github.com>
2024-08-06 21:53:16 +0900

    Fix [FE] : 주석 제거

---
+++ FE/pet-bridge/src/components/common/OpenPopover.jsx
@@ -0,0 +1,5 @@
+const openPopover = () => {
+  return <div className="asolute top-20"></div>
+}
+
+export default openPopover
--- FE/pet-bridge/src/pages/BoardPage.jsx
+++ FE/pet-bridge/src/pages/BoardPage.jsx
@@ -10,26 +10,6 @@ const categories = [
   {id: 4, title: "공지사항"},
 ]
 
-// const NavItem = ({category, href}) => {
-//   return (
-//     <li className="flex h-12 cursor-pointer items-center px-2.5 text-xl">
-//       <Link to={href}>{category}</Link>
-//     </li>
-//   )
-// }
-
-// const NavItemList = () => {
-//   return (
-//     <ul className="flex h-12 items-center">
-//       <NavLogo />
-//       <NavItem category={"입양홍보"} href={"/adoption-promotion"} />
-//       <NavItem category={"입양후기"} href={"/adoption-reviews"} />
-//       <NavItem category={"자유게시판"} href={"/freeboard"} />
-//       <NavItem category={"공지사항"} href={"/noticeboard"} />
-//     </ul>
-//   )
-// }
-
 const BoardNavbar = () => {
   const navigate = useNavigate()
   const handleNavigation = (bcode) => {
