ca879ee0279b17932f0c795461a43dde98d0f874
이다영 <120040559+2dayoung@users.noreply.github.com>
2024-08-07 14:06:22 +0900

    Feat [FE] : 오픈비두 demo 적용

--- FE/pet-bridge/src/index.js
+++ FE/pet-bridge/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals"
 import {BrowserRouter} from "react-router-dom"
 import store from "store"
 import {Provider} from "react-redux"
+import registerServiceWorker from "./registerServiceWorker"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
@@ -15,5 +16,5 @@ root.render(
     </BrowserRouter>
   </Provider>
 )
-
+registerServiceWorker()
 reportWebVitals()
