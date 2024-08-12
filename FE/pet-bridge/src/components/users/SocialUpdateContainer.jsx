4ea4b658c36f16a14642a3a83f53e2604d5aeb20
김재훈 <kgarry@naver.com>
2024-08-12 11:17:13 +0900

    Feat [FE] : 소셜로그인 유저 정보 수정 폼 FormData 양식으로 변경

--- FE/pet-bridge/src/components/users/SocialUpdateContainer.jsx
+++ FE/pet-bridge/src/components/users/SocialUpdateContainer.jsx
@@ -63,8 +63,14 @@ const SocialUpdateContainer = () => {
     // 에러가 없다면
     if (Object.values(newErrors).every((value) => value === "")) {
       // patch 비동기 요청
+      const formData = new FormData()
+      formData.append(
+        "userEditRequestDto",
+        new Blob([JSON.stringify(updateFormData)], {type: "application/json"})
+      )
+
       try {
-        const res = await patchUserInfo(updateFormData)
+        const res = await patchUserInfo(formData)
         navigate("/users/social/success")
         console.log(res)
         return
